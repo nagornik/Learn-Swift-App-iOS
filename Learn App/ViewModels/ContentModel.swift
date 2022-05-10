a654b3d28838741c968d29bcfe8f22b5f009b3de
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-10 19:15:50 +0300

    – Add assets – Add data files – Add models – Parse json and style.html

--- Learn App/ViewModels/ContentModel.swift
+++ Learn App/ViewModels/ContentModel.swift
@@ -9,4 +9,37 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData() {
+        if let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json") {
+            do {
+                let rawData = try Data(contentsOf: jsonUrl)
+                let decodedData = try JSONDecoder().decode([Module].self, from: rawData)
+                self.modules = decodedData
+                print("yes")
+            } catch {
+                print("no")
+            }
+        }
+        
+        if let htmlUrl = Bundle.main.url(forResource: "style", withExtension: "html") {
+            do {
+                let rawStyle = try Data(contentsOf: htmlUrl)
+                styleData = rawStyle
+                print("style yes")
+            } catch {
+                print("style no")
+            }
+        }
+        
+    }
+    
 }
