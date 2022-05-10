9aed2b4bcfff340d49ec53f760a8ab4efa791cb9
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-10 18:15:42 +0300

    Initial Commit

---
+++ Learn App/Learn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_AppApp.swift
+//  Learn App
+//
+//  Created by Anton Nagornyi on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Learn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
